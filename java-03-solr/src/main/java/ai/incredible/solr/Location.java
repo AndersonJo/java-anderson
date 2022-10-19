e92e68aa29a2149982a4b4a847d31d5ea068200f
Anderson <anderson.pytorch@gmail.com>
2022-10-20 03:17:26 +0900

    fgf

---
+++ java-03-solr/src/main/java/ai/incredible/solr/Location.java
@@ -0,0 +1,14 @@
+package ai.incredible.solr;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Location {
+
+    private String name;
+    private double lat;
+    private double lng;
+
+}
--- java-03-solr/src/test/java/ai/incredible/solr/SolrTest.java
+++ java-03-solr/src/test/java/ai/incredible/solr/SolrTest.java
@@ -3,6 +3,7 @@ package ai.incredible.solr;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.google.gson.Gson;
+
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
@@ -10,6 +11,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Objects;
 import java.util.stream.Collectors;
+
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
@@ -23,6 +25,7 @@ import org.junit.jupiter.api.Test;
 /**
  * 시작전에 다음을 실행.
  * ./bin/solr create -c users
+ * ./bin/solr start
  *
  * 삭제는..
  * rm -rf server/solr/users/
@@ -52,10 +55,24 @@ class SolrTest {
         }
     }
 
+    Location[] getLocationData() {
+        try (Reader reader = new InputStreamReader(Objects.requireNonNull(getClass()
+                .getClassLoader()
+                .getResourceAsStream("geodata.json")))) {
+
+            Location[] data = new Gson().fromJson(reader, Location[].class);
+            return data;
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+
     @Test
     void testSimpleQuerying() throws SolrServerException, IOException {
         // Input a document
-        ArrayList<Long> bookIds = new ArrayList<Long> (Arrays.asList(1L, 2L, 3L, 4L, 5L));
+        ArrayList<Long> bookIds = new ArrayList<Long>(Arrays.asList(1L, 2L, 3L, 4L, 5L));
 
         SolrInputDocument document = new SolrInputDocument();
         document.addField("id", "123456");
@@ -88,10 +105,10 @@ class SolrTest {
             // 처리 방식이 다르다. Collection<String> 으로 되어 있음
             ArrayList<Long> values = doc.getFieldValues("bookIds")
                     .stream()
-                    .map(c -> Long.parseLong((String)c))
+                    .map(c -> (Long) c)
                     .collect(Collectors.toCollection(ArrayList::new));
-            for(int i=0; i < values.size(); i++){
-                assertEquals(i+1, values.get(i));
+            for (int i = 0; i < values.size(); i++) {
+                assertEquals(i + 1, values.get(i));
             }
 
             // Collection<Long> 으로 되어 있음.
@@ -99,8 +116,8 @@ class SolrTest {
                     .stream()
                     .map(c -> (Long) c)
                     .collect(Collectors.toCollection(ArrayList::new));
-            for(int i=0; i < values.size(); i++){
-                assertEquals((i+1) * 10L, values2.get(i));
+            for (int i = 0; i < values.size(); i++) {
+                assertEquals((i + 1) * 10L, values2.get(i));
             }
 
             // Test setField for duplicate array.
@@ -133,7 +150,6 @@ class SolrTest {
         query.set("q", "cum:(500 100)");
         response = client.query(query);
         assertEquals(0, response.getResults().size());
-
 
 
         client.deleteByQuery("*:*");
@@ -191,11 +207,11 @@ class SolrTest {
 //        System.out.println(responseDocument.getFieldValues("status2"));
 
         ArrayList<String> status1 = responseDocument.getFieldValues("status1")
-                .stream().map(v -> (String)v)
+                .stream().map(v -> (String) v)
                 .collect(Collectors.toCollection(ArrayList::new));
 
         ArrayList<String> status2 = responseDocument.getFieldValues("status2")
-                .stream().map(v -> (String)v)
+                .stream().map(v -> (String) v)
                 .collect(Collectors.toCollection(ArrayList::new));
 
         assertEquals(1, status1.size());
@@ -204,6 +220,51 @@ class SolrTest {
         // Delete
         client.deleteByQuery("*:*");
         client.commit();
+    }
+
+    @Test
+    void testGeoFilter() throws SolrServerException, IOException {
+        Location[] data = getLocationData();
+        for(Location loc : data){
+            SolrInputDocument document = new SolrInputDocument();
+            document.setField("name", loc.getName());
+            document.setField("lat", loc.getLat());
+            document.setField("lng", loc.getLng());
+            client.add(document);
+        }
+        client.commit();
+        SolrQuery query = new SolrQuery();
+        query.set("q", "{!geofilt d=10}&pt=37.499402,127.054207");
+        QueryResponse response = client.query(query);
+
+        for(SolrDocument doc:response.getResults()){
+            System.out.println(doc.getFieldValue("name"));
+        }
+
+
+
+
+    }
+
+
+    public static double distance(double lat1, double lng1, double lat2,
+                                  double lng2, double el1, double el2) {
+
+        final int R = 6371; // Radius of the earth
+
+        double latDistance = Math.toRadians(lat2 - lat1);
+        double lonDistance = Math.toRadians(lng2 - lng1);
+        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+                + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
+                * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double distance = R * c * 1000; // convert to meters
+
+        double height = el1 - el2;
+
+        distance = Math.pow(distance, 2) + Math.pow(height, 2);
+
+        return Math.sqrt(distance);
     }
 
 
